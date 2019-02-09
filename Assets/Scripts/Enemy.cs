5084c55358f1bb55b89e1b23790ebc08b976b9ee
joebinalinbing <40000544+joebinalinbing@users.noreply.github.com>
2019-02-09 01:25:40 -0800

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    void Start()
+    {
+
+
+        target = Waypoints.points[0];
+
+
+    }
+
+    void Update() {
+
+
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized*speed*Time.deltaTime, Space.World);
+    }
+
+
+
+}
