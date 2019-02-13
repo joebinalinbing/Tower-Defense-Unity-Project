c6e22850cf2b34b90669062a68f2a48295c380f4
joebinalinbing <40000544+joebinalinbing@users.noreply.github.com>
2019-02-12 21:39:38 -0800

    yes
    si

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -21,8 +21,22 @@ public class Enemy : MonoBehaviour
 
         Vector3 dir = target.position - transform.position;
         transform.Translate(dir.normalized*speed*Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f) {
+            GetNextWaypoint();
+        }
     }
 
+    void GetNextWaypoint() {
+        if(wavepointIndex >= Waypoints.points.Length - 1)
+        {
 
+            Destroy(gameObject);
+            return;
+        }
+        wavepointIndex++;
+        target = Waypoints.points[wavepointIndex];
+
+    }
 
 }
